17c69d7a791021f3c911471189d662ec89488127
Yash_rajankar <yashrajankar67@gmail.com>
2026-05-28 22:18:20 +0530

    Initial commit

---
+++ Pract1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pract1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row:");
+        int row = sc.nextInt();
+        System.out.println("Enter col:");
+        int col = sc.nextInt();
+
+        for(int i = 1;i <= row;i++){
+            for(int j=1;j<= col;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
